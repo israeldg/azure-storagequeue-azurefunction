69a54a4c917132a4791f5080c8eaf97d28e29fcc
Israel De La Rosa <jose.israel@cambaycs.com>
2023-07-08 19:04:34 -0500

    consoleappadded

---
+++ consoleDefaultAzureIdentity/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Identity; 
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            var credential = new DefaultAzureCredential();
+              Console.WriteLine("Hello, World!");
+        }
+    }
+}
